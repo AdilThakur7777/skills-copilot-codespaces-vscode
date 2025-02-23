e8538d5f7f04930f8b92b8e8c80b1ff26215955d
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2025-02-23 07:34:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,54 +1,19 @@
-//create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var qs = require('querystring');
-var comments = [];
-var server = http.createServer(function(req, res){
-  //parse url
-  var parseUrl = url.parse(req.url);
-  var pathName = parseUrl.pathname;
-  if(pathName === '/'){
-    fs.readFile('./index.html', function(err, data){
-      if(err){
-        console.log(err);
-        res.end('404 Not Found');
-      }else{
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        res.end(data);
-      }
-    });
-  }else if(pathName === '/comment'){
-    if(req.method === 'POST'){
-      var postData = '';
-      req.on('data', function(chunk){
-        postData += chunk;
-      });
-      req.on('end', function(){
-        var comment = qs.parse(postData);
-        comments.push(comment.comment);
-        res.end('success');
-      });
-    }else{
-      res.end(JSON.stringify(comments));
-    }
-  }else{
-    fs.readFile(path.join(__dirname, pathName), function(err, data){
-      if(err){
-        console.log(err);
-        res.end('404 Not Found');
-      }else{
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        res.end(data);
-      }
-    });
-  }
+// Create web server
+// 1. Import express
+const express = require('express');
+// 2. Create express server
+const app = express();
+// 3. Create a port
+const PORT = 3000;
+// 4. Listen to port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-server.listen(3000, function(){
-  console.log('server is listening on port 3000');
+// 5. Create a route
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+// 6. Create another route
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page');
 });
