e18dea4b419e8e109d60e4cfd60b5d2c2c7e7f7e
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2025-02-23 07:31:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,54 @@
 //create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-const fs = require('fs');
-
-//get data
-app.get('/comments', (req, res) => {
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        res.send(data);
-    });
-});
-
-//post data
-app.post('/comments', (req, res) => {
-    let body = '';
-    req.on('data', (chunk) => {
-        body += chunk;
-    }).on('end', () => {
-        fs.readFile('comments.json', 'utf8', (err, data) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            }
-            let comments = JSON.parse(data);
-            let newComment = JSON.parse(body);
-            comments.push(newComment);
-            fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send('Internal Server Error');
-                }
-                res.send('Comment added');
-            });
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var qs = require('querystring');
+var comments = [];
+var server = http.createServer(function(req, res){
+  //parse url
+  var parseUrl = url.parse(req.url);
+  var pathName = parseUrl.pathname;
+  if(pathName === '/'){
+    fs.readFile('./index.html', function(err, data){
+      if(err){
+        console.log(err);
+        res.end('404 Not Found');
+      }else{
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
         });
+        res.end(data);
+      }
     });
+  }else if(pathName === '/comment'){
+    if(req.method === 'POST'){
+      var postData = '';
+      req.on('data', function(chunk){
+        postData += chunk;
+      });
+      req.on('end', function(){
+        var comment = qs.parse(postData);
+        comments.push(comment.comment);
+        res.end('success');
+      });
+    }else{
+      res.end(JSON.stringify(comments));
+    }
+  }else{
+    fs.readFile(path.join(__dirname, pathName), function(err, data){
+      if(err){
+        console.log(err);
+        res.end('404 Not Found');
+      }else{
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        res.end(data);
+      }
+    });
+  }
 });
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+server.listen(3000, function(){
+  console.log('server is listening on port 3000');
 });
